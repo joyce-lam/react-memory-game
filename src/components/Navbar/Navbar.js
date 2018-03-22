ac7381949548840c15d289b7d928a69503ee8b20
Joyce <joycelamsaungan@gmail.com>
2018-03-21 20:53:04 -0700

    adding Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = props => (
+  <nav className="navbar navbar-default">
+  	<div className="container-fluid">
+  		<div className="navbar-header">
+  			<div className="navbar-brand">
+          <h2>Memory Game</h2>
+        </div>
+  		</div>
+  		<ul className="navbar-nav navbar-right text-center">
+  			<li ><h3>Click an image to begin</h3></li>      
+        <li><h3>Score:{props.score}</h3></li>
+        <li><h3>Top Score: {props.highestScore}</h3></li>
+        </ul>
+    </div>
+  </nav>
+);
+
+
+
+export default Navbar;
+
+
