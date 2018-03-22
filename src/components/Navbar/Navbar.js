ead1fb7e33bfed5ac293d36c5f0485657306afc2
Joyce <joycelamsaungan@gmail.com>
2018-03-21 21:26:54 -0700

    modifying nav

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,16 +9,13 @@ const Navbar = props => (
           <h2>Memory Game</h2>
         </div>
   		</div>
-  		<ul className="navbar-nav navbar-right text-center">
-  			<li ><h3>Click an image to begin</h3></li>      
+  		<ul className="navbar-nav navbar-right text-center">  
         <li><h3>Score:{props.score}</h3></li>
         <li><h3>Top Score: {props.highestScore}</h3></li>
         </ul>
     </div>
   </nav>
 );
-
-
 
 export default Navbar;
 
