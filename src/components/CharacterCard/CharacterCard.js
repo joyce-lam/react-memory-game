193934b6e4ce7b0eba5db6206c2beb80afe62321
Joyce <joycelamsaungan@gmail.com>
2018-03-21 21:24:36 -0700

    modifying component

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./CharacterCard.css";
-// import { Grid, Row} from 'react-bootstrap';
 
 const CharacterCard = props => (
-	<div className="col-xs-12 col-sm-6 col-md-4 card">
+	<div className="col-xs-12 col-sm-6 col-md-4 card text-center">
 		<div className="img-container" data-id={props.id} onClick={()=> props.checkClick(props.id)}>
 				<img alt={props.name} src={props.image} />
 		</div>
