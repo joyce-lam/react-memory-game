ae9888928199a04ce4b3e9aa3df8d3c14fcd4c16
Joyce <joycelamsaungan@gmail.com>
2018-03-21 20:06:49 -0700

    adding bootstrap grid

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./CharacterCard.css";
+// import { Grid, Row} from 'react-bootstrap';
 
 const CharacterCard = props => (
-	<div className="card">
+	<div className="col-xs-12 col-sm-6 col-md-4 card">
 		<div className="img-container" data-id={props.id} onClick={()=> props.checkClick(props.id)}>
-			<img alt={props.name} src={props.image} />
+				<img alt={props.name} src={props.image} />
 		</div>
 	</div>
 );
